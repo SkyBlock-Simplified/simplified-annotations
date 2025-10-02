096e055a6e8cf209a2061baa8c148dc827c74b7c
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-10-01 22:07:29 -0400

    Fix heavy lag in 800+ line files

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "dev.sbs"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
@@ -39,7 +39,7 @@ intellijPlatform {
         }
 
         changeNotes = """
-      Enable inspection by default, set default highlight level, do not scan files during indexing
+      Fix heavy lag in 800+ line files
     """.trimIndent()
     }
     buildSearchableOptions = false
